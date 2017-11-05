e48f456602417ef3071970a4a9df28f41d66bc4f
dpham4294 <dpham4294@gmail.com>
2017-11-05 13:30:09 -0500

    Register/Homepage/Servlets

---
+++ OnPoint/src/main/java/onpoint/servlet/RegisterServlet.java
@@ -0,0 +1,33 @@
+package onpoint.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+public class RegisterServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		resp.setContentType("text/html");
+		req.getRequestDispatcher("register.html").include(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		
+		resp.setContentType("text/html");
+		
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		String email = req.getParameter("email");
+		String phone_number = req.getParameter("phone_number");
+		
+		resp.sendRedirect(req.getContextPath() 
+				+ "/homepage.html");
+	}
+}
