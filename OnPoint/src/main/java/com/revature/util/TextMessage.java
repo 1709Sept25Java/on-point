3862792e3b911535d153dcb841d17074eca4de50
agarnelo <garnelo.anahi@gmail.com>
2017-11-10 14:03:05 -0500

    Scheduling task

--- OnPoint/src/main/java/com/revature/util/TextMessage.java
+++ OnPoint/src/main/java/com/revature/util/TextMessage.java
@@ -5,14 +5,14 @@ import com.twilio.type.PhoneNumber;
 
 
 public class TextMessage {
-	 public static final String ACCOUNT_SID = "";
-	  public static final String AUTH_TOKEN = "";
-	  
-	  public static Boolean sendTextNotification(String toNumber, String notification) {
+	 public static final String ACCOUNT_SID = "AC58e89ff1208bb617f0e9d27742bab685";
+	 public static final String AUTH_TOKEN = "97ea83e31e5c107fe161e463a4fd4beb";
+	 public static final String FROM_NUMBER = "+13479604243";
+	 public static Boolean sendTextNotification(String toNumber, String notification) {
 		  Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 		  try {
 			  Message message = Message.creator(new PhoneNumber(toNumber),
-				        new PhoneNumber("+"), 
+				        new PhoneNumber(FROM_NUMBER), 
 				        notification).create();
 			  message.getSid();
 			  return true;
@@ -21,18 +21,18 @@ public class TextMessage {
 			  return false;
 		  }
 	  }
+	 
 	  public static Boolean sendTextNotificationWithImage(String toNumber, String notification, String url) {
 		  Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 		  try {
 			  Message message = Message.creator(new PhoneNumber(toNumber),
-				        new PhoneNumber("+"), 
+				        new PhoneNumber(FROM_NUMBER), 
 				        notification)
 					  .setMediaUrl(url)
 					  .create();
 			  message.getSid();
 			  return true;
-		  }
-		  catch(Exception e) {
+		  } catch(Exception e) {
 			  return false;
 		  }
 	  }
