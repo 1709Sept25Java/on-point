8040f782a5e9094ba3c4c3713ccb2597f8257c84
dpham4294 <dpham4294@gmail.com>
2017-11-13 09:27:41 -0500

    save phone number

--- OnPoint/src/main/java/com/revature/controllers/LoginController.java
+++ OnPoint/src/main/java/com/revature/controllers/LoginController.java
@@ -51,7 +51,7 @@ public class LoginController {
 	}
 
 	@RequestMapping(value = "/homepage")
-	public ModelAndView loginProcess(HttpServletRequest request, HttpServletResponse response,
+	public ModelAndView loginProcess(HttpSession session, HttpServletRequest request, HttpServletResponse response,
 	@ModelAttribute("login") Login login) {
 	  ModelAndView mav = null;
 	  String username = login.getUsername();
@@ -79,6 +79,7 @@ public class LoginController {
 	  }
 	  try {
 		saveAllEvents(request, response);
+		saveUser(session);
 	} catch (IOException e) {
 		e.printStackTrace();
 	}
